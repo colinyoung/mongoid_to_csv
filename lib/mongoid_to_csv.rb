9a93f9daca5e669a475af83dcd0836dbe09ee694
Colin Young <me@colinyoung.com>
2013-05-29 19:16:54 -0500

    Two updates I needed for my project.
    - Ability to override the self.to_csv method to add fields as an array.
    
    ex.
    
    class MyModel
      include Mongoid::Document
    
      def self.to_csv
        documents_to_csv all, %w(name address1 location)
      end
    end
    
    - Supports embedded documents. That is, you can use keys with dots in them: (ex: 'rating.stars').
    - Finally, removes the newline at the end of Mongoid values for fields that are Arrays.

--- lib/mongoid_to_csv.rb
+++ lib/mongoid_to_csv.rb
@@ -6,7 +6,7 @@ module MongoidToCSV
   # Return full CSV content with headers as string.
   # Defined as class method which will have chained scopes applied.
   def to_csv
-    documents_to_csv(all, fields)
+    documents_to_csv all
   end
 
   module_function
@@ -14,19 +14,39 @@ module MongoidToCSV
   def documents_to_csv(documents, fields = nil)
     return documents.to_csv unless documents.first.class.respond_to? :fields
     
-    fields ||= documents.first.class.fields
+    fields ||= documents.first.class.fields.keys
     
     doc_class = documents.first.class
-    csv_columns = fields.keys - %w{_id created_at updated_at _type}
+    csv_columns = fields - %w{_id created_at updated_at _type}
     header_row = csv_columns.to_csv
     records_rows = documents.map do |record|
       csv_columns.map do |column|
-        value = record.send(column)
-        value = value.to_csv if value.respond_to?(:to_csv)
+        value = if column.include? '.'
+          column.split('.').reduce(record) {|r, method| r.send(method) }
+        else
+          record.send(column)
+        end
+        
+        if value.respond_to?(:to_csv)
+          value = value.to_csv
+          value.gsub! /\n$/, '' if array_type_for_record(record, column)
+        else
+          value.to_s
+        end
+
         value
       end.to_csv
     end.join
     header_row + records_rows
+  end
+
+  private
+
+  def array_type_for_record record, column
+    embed_list = column.split('.')
+    field = embed_list.pop
+    instance = embed_list.reduce(record) {|r, method| r.send(method) }
+    instance.class.fields[field].type == Array
   end
 
 end
